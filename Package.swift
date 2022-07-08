0ff3717d02e65ace69eec8891731114132f7d2b7
Pop Flamingo <8317047+PopFlamingo@users.noreply.github.com>
2022-07-08 07:33:02 +0000

    Add swift-fly-app-discovery dependency

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-distributed-actors.git", revision: "1.0.0-beta.1.1"),
+        .package(url: "https://github.com/PopFlamingo/swift-fly-app-discovery.git", revision: "0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
