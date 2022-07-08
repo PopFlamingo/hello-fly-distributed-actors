7d747301faf00741ecd0af46b34b6636653ab340
Pop Flamingo <8317047+PopFlamingo@users.noreply.github.com>
2022-07-08 08:57:22 +0000

    Try to use selfIP

--- Sources/HelloFlyDistributedActors/HelloFlyDistributedActors.swift
+++ Sources/HelloFlyDistributedActors/HelloFlyDistributedActors.swift
@@ -9,7 +9,9 @@ public struct HelloFlyDistributedActors {
     public static func main() async throws {
         let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
         var settings = ClusterSystemSettings.default
+        
         let discovery = try await FlyAppDiscovery(eventLoopGroup: eventLoopGroup, port: 7337)
+        settings.bindHost = discovery.selfIP
         settings.logging.logLevel = .debug
         settings.discovery = ServiceDiscoverySettings(discovery, service: .currentApp())
         let system = await ClusterSystem("HelloCluster", settings: settings)
