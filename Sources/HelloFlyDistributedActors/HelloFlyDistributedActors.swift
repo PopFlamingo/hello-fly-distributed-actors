156a9e4aa116bed5d2cd3e7372ef3adf6a5e18d4
Pop Flamingo <8317047+PopFlamingo@users.noreply.github.com>
2022-07-08 07:59:44 +0000

    Create minimalistic cluster

--- Sources/HelloFlyDistributedActors/HelloFlyDistributedActors.swift
+++ Sources/HelloFlyDistributedActors/HelloFlyDistributedActors.swift
@@ -1,8 +1,19 @@
+import Distributed
+import DistributedActors
+import FlyAppDiscovery
+import NIO
+
 @main
 public struct HelloFlyDistributedActors {
-    public private(set) var text = "Hello, World!"
 
-    public static func main() {
-        print(HelloFlyDistributedActors().text)
+    public static func main() async throws {
+        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        var settings = ClusterSystemSettings.default
+        let discovery = try await FlyAppDiscovery(eventLoopGroup: eventLoopGroup, port: 7337)
+        settings.discovery = ServiceDiscoverySettings(discovery, service: .currentApp())
+        let system = await ClusterSystem("HelloCluster", settings: settings)
+        for await event in system.cluster.events {
+            print(event)
+        }
     }
 }
