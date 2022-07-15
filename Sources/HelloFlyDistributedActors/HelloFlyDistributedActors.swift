a1b23a6b350c3078eae19e4294f547c0cd196541
Pop Flamingo <8317047+PopFlamingo@users.noreply.github.com>
2022-07-15 07:43:42 +0000

    Just some test

--- Sources/HelloFlyDistributedActors/HelloFlyDistributedActors.swift
+++ Sources/HelloFlyDistributedActors/HelloFlyDistributedActors.swift
@@ -8,19 +8,23 @@ import Foundation
 public struct HelloFlyDistributedActors {
 
     public static func main() async throws {
-        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        var settings = ClusterSystemSettings.default
+        let task = Task.detached {
+            let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+             var settings = ClusterSystemSettings.default
         
-        let discovery = try await FlyAppDiscovery(eventLoopGroup: eventLoopGroup, port: 7337)
-        settings.bindHost = discovery.selfIP
-        settings.logging.logLevel = .error
-        settings.discovery = ServiceDiscoverySettings(discovery, service: .currentApp())
-        let system = await ClusterSystem("HelloCluster", settings: settings)
-        for await event in system.cluster.events {
-            print("\(time()) - Got event:", event)
-            print("\(time()) - Will now get a membership snapshot...")
-            print("\(time()) - Got snapshot:", await system.cluster.membershipSnapshot)
+            let discovery = try await FlyAppDiscovery(eventLoopGroup: eventLoopGroup, port: 7337)
+            settings.bindHost = discovery.selfIP
+            settings.logging.logLevel = .error
+            settings.discovery = ServiceDiscoverySettings(discovery, service: .currentApp())
+            let system = await ClusterSystem("HelloCluster", settings: settings)
+            for await event in system.cluster.events {
+                print("\(time()) - Got event:", event)
+                print("\(time()) - Will now get a membership snapshot...")
+                print("\(time()) - Got snapshot:", await system.cluster.membershipSnapshot)
+            }
         }
+        try await task.value
+        
     }
 }
 
